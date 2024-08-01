d428d285a29a1da8d3155140d0371dfa0bfb21d3
KaterynaBachkalo <kozlovets88@gmail.com>
2024-08-01 15:38:38 +0300

    add click

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,6 +2,7 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const menuBtn = document.querySelectorAll('.link');
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -12,6 +13,9 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  menuBtn.forEach(item => {
+    item.addEventListener('click', toggleMenu);
+  });
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 1440px)').addEventListener('change', e => {
